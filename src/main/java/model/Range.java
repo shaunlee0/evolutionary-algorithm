8fe601a17ea7b92449dbe593cc0243983c5b17a7
shaunlee0 <shaunlee0@googlemail.com>
2016-11-11 12:53:56 +0000

    Add Range class

---
+++ src/main/java/model/Range.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Range {
+    double[] values;
+
+    public Range(double value1, double value2){
+        values = new double[2];
+        if (value1<value2){
+            values[0] = value1;
+            values[1] = value2;
+        }else{
+            values[0] = value2;
+            values[1] = value1;
+        }
+    }
+}
